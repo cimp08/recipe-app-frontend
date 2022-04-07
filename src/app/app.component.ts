8545027d3fed0c9dfd2578f2f1a00e10003aee59
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-07 13:09:26 +0200

    import AuthService and add logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './shared/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'u06-recipe-frontend-final';
+  constructor(public authService: AuthService) {}
+  logout() {
+    this.authService.doLogout();
+  }
 }
