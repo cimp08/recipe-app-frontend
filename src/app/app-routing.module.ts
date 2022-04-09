9cea0c18bb3b7b766a7fbf9313ef380a3358a3ad
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-09 11:24:57 +0200

    add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { IndexComponent } from './components/recipe/index/index.component';
+import { RecipedetailComponent } from './components/recipe/recipedetail/recipedetail.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+  { path: '', redirectTo: '/recipe', pathMatch: 'full' },
   { path: 'log-in', component: SigninComponent },
   { path: 'sign-up', component: SignupComponent },
+  { path: 'recipe', component: IndexComponent },
+  { path: 'recipedetail/:id', component: RecipedetailComponent },
   {
     path: 'user-profile/:id',
     component: UserProfileComponent,
