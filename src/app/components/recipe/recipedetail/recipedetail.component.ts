09c5df4961cced08327358b99b06cf3e5f9fad0b
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-10 19:17:50 +0200

    add getalllists and addRecipeToList function

--- src/app/components/recipe/recipedetail/recipedetail.component.ts
+++ src/app/components/recipe/recipedetail/recipedetail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from 'src/app/shared/recipe.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { List } from 'src/app/shared/list';
+import { Recipe } from 'src/app/shared/recipe';
+import { ListService } from 'src/app/shared/list.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipedetail',
@@ -8,14 +13,18 @@ import { RecipeService } from 'src/app/shared/recipe.service';
   styleUrls: ['./recipedetail.component.css'],
 })
 export class RecipedetailComponent implements OnInit {
+  /* form!: FormGroup; */
   recipe!: any;
+  lists: List[] = [];
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public authService: AuthService,
+    public listService: ListService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.recipeService
       .getRecipe(this.route.snapshot.params['id'])
       .subscribe((data) => {
@@ -23,5 +32,38 @@ export class RecipedetailComponent implements OnInit {
         console.log(this.recipe);
         return this.recipe;
       });
+
+    this.listService
+      .getAll(localStorage.getItem('user_id'))
+      .subscribe((data: List[]) => {
+        this.lists = data;
+        console.log(this.lists);
+      });
+
+    /* this.form = new FormGroup({
+      recipe_api_id: new FormControl(''),
+      label: new FormControl(''),
+      photo_url: new FormControl(''),
+      log_id: new FormControl(''),
+    }); */
   }
+
+  addRecipeToList(listId: number) {
+    const recipeToPost = {
+      recipe_api_id: this.recipe.uri.slice(44),
+      label: this.recipe.label,
+      photo_url: this.recipe.image,
+      log_id: listId,
+    };
+    this.listService.saveRecipe(recipeToPost).subscribe((res: any) => {
+      console.log('Recipe saved successfully!');
+    });
+  }
+
+  /* submit() {
+    console.log(this.form.value);
+    this.listService.saveRecipe(this.form.value).subscribe((res: any) => {
+      console.log('Recipe saved successfully!');
+    });
+  } */
 }
