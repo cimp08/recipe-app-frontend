edc6fcd2f966b76bfbf792eeba65fd7f241b1799
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-11 17:28:17 +0200

    clean code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { IndexComponent } from './components/recipe/index/index.component';
 import { RecipedetailComponent } from './components/recipe/recipedetail/recipedetail.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
@@ -14,11 +12,6 @@ const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
   { path: 'recipe', component: IndexComponent },
   { path: 'recipedetail/:id', component: RecipedetailComponent },
-  /* {
-    path: 'user-profile/:id',
-    component: UserProfileComponent,
-    canActivate: [AuthGuard],
-  }, */
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
